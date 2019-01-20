43e8897793494b6d2cde9940b887cbf5a6ca323e
Mike King <michael.y.king@nab.com.au>
2019-01-21 00:08:29 +1100

    Fixed duplicate folder rename. Running 100%

--- cli.test.js
+++ cli.test.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const mkpath = require('mkpath')
-const { execSync } = require('child_process')
+const fs = require("fs");
+const mkpath = require("mkpath");
+const { execSync } = require("child_process");
 
 // base is defined as a location outside a git repo. Assumes I have write
 // access to .. :)
-const base = '../.kebab-ify-test'
+const base = "../.kebab-ify-test";
 const widgetContents = `
 //# 
 //# Test file
@@ -17,7 +17,7 @@ import SomeThingElse from '../../components/SomeThingElse'
 //
 // The rest of the file isn't important
 //
-`
+`;
 const avatarContents = `
 //# 
 //# Avatar component test file
@@ -26,52 +26,52 @@ import SomeThing from '../../components/SomeThing'
 //
 // The rest of the file isn't important
 //
-`
+`;
 
 afterAll(() => {
-  // execSync(`rm -rf ${base}`)
-})
+  execSync(`rm -rf ${base}`);
+});
 
 //
 // Defensive cleanup of old test folder
 //
 if (fs.existsSync(base)) {
-  execSync(`rm -rf ${base}/*`)
+  execSync(`rm -rf ${base}/*`);
 }
-mkpath.sync(`${base}/src/Components/MyWidget`)
-const widgetsrc = `${base}/src/Components/MyWidget/MyWidget.js`
+mkpath.sync(`${base}/src/Components/MyWidget`);
+const widgetsrc = `${base}/src/Components/MyWidget/MyWidget.js`;
 fs.writeFileSync(widgetsrc, widgetContents, {
-  encoding: 'utf8'
-})
-const avatarsrc = `${base}/src/Components/Avatar.js`
+  encoding: "utf8"
+});
+const avatarsrc = `${base}/src/Components/Avatar.js`;
 fs.writeFileSync(avatarsrc, avatarContents, {
-  encoding: 'utf8'
-})
+  encoding: "utf8"
+});
 
-test('Files are set up', async () => {
-  await expect(fs.existsSync(base)).toBeTruthy()
-  await expect(fs.existsSync(widgetsrc)).toBeTruthy()
-})
+test("Files are set up", async () => {
+  await expect(fs.existsSync(base)).toBeTruthy();
+  await expect(fs.existsSync(widgetsrc)).toBeTruthy();
+});
 
-test('Kebab-ify!!!', () => {
-  expect(execSync('kebab-ify src', { cwd: base })).toBeTruthy()
-})
+test("Kebab-ify!!!", () => {
+  expect(execSync("kebab-ify src", { cwd: base })).toBeTruthy();
+});
 
-test('PascalCase files are not there any more', () => {
-  expect(fs.existsSync(base)).toBeTruthy()
+test("PascalCase files are not there any more", () => {
+  expect(fs.existsSync(base)).toBeTruthy();
   // Because MacOS isn't case sensitive, this folder appears to be there, even though it
   // has been renamed, so we can't do this test
   // expect(fs.existsSync(`${base}/src/Components`)).toBeFalsy()
-  expect(fs.existsSync(`${base}/src/Components/MyWidget`)).toBeFalsy()
+  expect(fs.existsSync(`${base}/src/Components/MyWidget`)).toBeFalsy();
   expect(
     fs.existsSync(`${base}/src/Components/MyWidget/MyWidget.js`)
-  ).toBeFalsy()
-})
+  ).toBeFalsy();
+});
 
-test('kebab-case files are there', () => {
-  expect(fs.existsSync(base)).toBeTruthy()
-  expect(fs.existsSync(`${base}/src/components`)).toBeTruthy()
+test("kebab-case files are there", () => {
+  expect(fs.existsSync(base)).toBeTruthy();
+  expect(fs.existsSync(`${base}/src/components`)).toBeTruthy();
   expect(
     fs.existsSync(`${base}/src/components/my-widget/my-widget.js`)
-  ).toBeTruthy()
-})
+  ).toBeTruthy();
+});
